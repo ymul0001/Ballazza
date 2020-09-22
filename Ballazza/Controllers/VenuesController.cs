04978ddcfde575e97aac500c013d410a51659166
Yonas Mulyadi <ymul0001@student.monash.edu>
2020-09-23 01:12:14 +0700

    add the map feature

---
+++ Ballazza/Controllers/VenuesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ballazza.Models;
+
+namespace Ballazza.Controllers
+{
+    public class VenuesController : Controller
+    {
+        private BallazzaEntities db = new BallazzaEntities();
+
+        // GET: Venues
+        public ActionResult Index()
+        {
+            return View(db.Venues.ToList());
+        }
+
+        [AllowAnonymous]
+        public ActionResult GetVenuesList()
+        {
+
+            var venuesList = db.Venues.ToList();
+            return Json(
+                new
+                {
+                    data = (from obj in venuesList
+                            select new
+                            {
+                                VenueName = obj.VenueName,
+                                VenueAddress = obj.VenueStreet + " " + obj.VenueSuburb + " " + obj.VenueState + " " + obj.VenuePostcode
+                            })
+                }, JsonRequestBehavior.AllowGet); ;
+        }
+
+        // GET: Venues/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Venue venue = db.Venues.Find(id);
+            if (venue == null)
+            {
+                return HttpNotFound();
+            }
+            return View(venue);
+        }
+
+        // GET: Venues/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Venues/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "VenueId,VenueName,VenueStreet,VenueSuburb,VenueState,VenuePostcode,VenuePhoneno")] Venue venue)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Venues.Add(venue);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(venue);
+        }
+
+        // GET: Venues/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Venue venue = db.Venues.Find(id);
+            if (venue == null)
+            {
+                return HttpNotFound();
+            }
+            return View(venue);
+        }
+
+        // POST: Venues/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "VenueId,VenueName,VenueStreet,VenueSuburb,VenueState,VenuePostcode,VenuePhoneno")] Venue venue)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(venue).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(venue);
+        }
+
+        // GET: Venues/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Venue venue = db.Venues.Find(id);
+            if (venue == null)
+            {
+                return HttpNotFound();
+            }
+            return View(venue);
+        }
+
+        // POST: Venues/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Venue venue = db.Venues.Find(id);
+            db.Venues.Remove(venue);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Ballazza/Scripts/venues.js
@@ -0,0 +1,74 @@
+﻿mapboxgl.accessToken = 'pk.eyJ1IjoieW9uczIyMjIiLCJhIjoiY2tmZGhhMmd2MDZ6MjJ5cDl2a2pnM2c0ciJ9.azj1qrJYfR2iaXXH4GQUSQ';
+
+
+/*
+ * Generating the map
+ * 
+ * */
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [144.970312785, -37.8068717725], // starting position
+    zoom: 11, // starting zoom
+});
+map.addControl(new mapboxgl.NavigationControl());
+
+
+/**
+ * Retrieving the location data from the back-end
+ * */
+$(document).ready(function () {
+    $.ajax({
+        url: "/Venues/getVenuesList",
+        type: 'GET',
+        dataType: 'json', // added data type
+        success: function (res) {
+            //console.log(res.data[1]);
+            for (let i = 0; i < res.data.length; i++) {
+                const data = res.data[i]
+                url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + data.VenueAddress + ".json?access_token=pk.eyJ1IjoieW9uczIyMjIiLCJhIjoiY2tmZGhhMmd2MDZ6MjJ5cDl2a2pnM2c0ciJ9.azj1qrJYfR2iaXXH4GQUSQ"
+                $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'json', // added data type
+                    success: function (res) {
+                        //console.log(res.features[0].geometry.coordinates)
+
+                        // create the popup information for each marker
+                        var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+                            '<h3>' + data.VenueName + '</h3><p>' + data.VenueAddress + '</p>'
+                        );
+
+                  
+                        // Create the marker
+                        marker = new mapboxgl.Marker({
+                            color: 'red'
+                        }).setLngLat(res.features[0].geometry.coordinates)
+                            .setPopup(popup)
+                            .addTo(map);
+                    }
+                });
+            }
+        }
+    });
+})
+
+//implement options to change the layer styles
+const switchLayer = (layer) => {
+    const layerId = layer.target.id;
+    map.setStyle('mapbox://styles/mapbox/' + layerId);
+}
+
+const menus = document.getElementById('menu').getElementsByTagName('input');
+
+for (let i = 0; i < menus.length; i++) {
+    menus[i].onclick = switchLayer;
+}
+
+
+document.getElementById('fly').addEventListener('click', function () {
+    map.flyTo({
+        center: [144.970312785, -37.8068717725],
+        essential: true 
+    });
+});
